ef92b84a8dca0cf08a0c3faabdf6221eb13394c9
Dean Padilla <d3anpadi@gmail.com>
2024-03-23 17:14:51 -0500

    exception

---
+++ src/main/java/com/dph/ms/siscova/exception/IllegalOperationException.java
@@ -0,0 +1,11 @@
+package com.dph.ms.siscova.exception;
+
+public class IllegalOperationException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public IllegalOperationException(String message) {
+		super (message);
+	}
+
+}
---
+++ src/main/java/com/dph/ms/siscova/services/VacunaService.java
@@ -0,0 +1,5 @@
+package com.dph.ms.siscova.services;
+
+public interface VacunaService {
+
+}
